25fa33861e0c0c19194d0d0dc6639aee34979684
Cost Granda <jorge_granda82112@elpoli.edu.co>
2016-05-16 23:22:59 -0500

    Primera entrega con el relacional sin corregir.

---
+++ Scripts/crear_esquema.sql
@@ -0,0 +1,24 @@
+--Borrar tablespace
+DROP TABLESPACE ts_compratodo INCLUDING CONTENTS AND DATAFILES;
+
+-- Crear tablespace con ubicación alternativa y tamaño de 10MB
+CREATE TABLESPACE ts_compratodo LOGGING
+DATAFILE 'C:\Users\coste\Documents\GitHub\BASES DE DATOS\PRIMER_DATAFILE.dbf' size 10M
+-- DATAFILE 'D:\PRIMER_DATAFILE.dbf' size 10M
+extent management local segment space management auto;
+
+--Borrar usuario
+DROP USER us_compratodo;
+
+-- Crear usuariom
+CREATE USER us_compratodo profile default
+IDENTIFIED BY 123456
+DEFAULT TABLESPACE ts_compratodo
+TEMPORARY TABLESPACE temp
+ACCOUNT UNLOCK;
+
+-- Permisos
+GRANT connect, resource, dba TO us_compratodo;
+
+-- Conectarse directamente
+connect us_compratodo/123456;
