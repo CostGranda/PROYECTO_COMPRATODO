22038c614985ba98b0bed86fa45243d82cc0e56d
Cost Granda <jorge_granda82112@elpoli.edu.co>
2016-06-02 21:31:35 -0500

    Se corrige el punto y coma faltante despues de la revisión

--- Scripts/INICIO.sql
+++ Scripts/INICIO.sql
@@ -1,4 +1,4 @@
 start C:\Users\coste\Documents\GitHub\PROYECTO_COMPRATODO\Scripts\crear_tablas.sql;
 start C:\Users\coste\Documents\GitHub\PROYECTO_COMPRATODO\Scripts\script_reglas.sql;
-start C:\Users\coste\Documents\GitHub\PROYECTO_COMPRATODO\Scripts\indices.sql
+start C:\Users\coste\Documents\GitHub\PROYECTO_COMPRATODO\Scripts\indices.sql;
 start C:\Users\coste\Documents\GitHub\PROYECTO_COMPRATODO\Scripts\inserts.sql;
